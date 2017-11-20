3a54dd19ea538421d17f0be127f061203c2286f4
Kelsey Chan <kelseykchan@gmail.com>
2017-11-20 08:14:59 -0500

    Add reset submissions button to judge screen
    - Also makes minor CSS changes

--- public/javascripts/judge.js
+++ public/javascripts/judge.js
@@ -1,3 +1,14 @@
 window.onload = function () {
   $('#next-round-btn').prop('disabled', !allSubmissionsReceived);
+
+  $('#reset-round-btn').click(function(event) {
+    var data = {
+      roundId: roundId
+    }
+
+    $.post('/gameplay/round/reset', data, function(res) {
+      console.log(res);
+      location.reload(true);
+    });
+  });
 }
