0bc1d3690a29cfbc7308bc50cb800f82306f45e0
Kelsey Chan <kelseykchan@gmail.com>
2017-11-25 11:48:39 -0800

    wip: Judging interface

--- public/javascripts/judge.js
+++ public/javascripts/judge.js
@@ -1,3 +1,9 @@
+// judge.js
+
+var Ditty = require('ditty');
+var ditty = Ditty();
+var Bopper = require('bopper');
+
 window.onload = function () {
-  $('#next-round-btn').prop('disabled', !allSubmissionsReceived);
+  // $('#next-round-btn').prop('disabled', !allSubmissionsReceived);
 }
